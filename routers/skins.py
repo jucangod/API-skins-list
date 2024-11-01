9e972a64b35573ad0e58e11f1aedc00d311696d4
Juan Carlos Espinoza <jc_espinozarodriguez@hotmail.com>
2024-10-31 19:12:08 -0600

    Ya se pueden actualizar los estados de skins y tambien se pueden ver las faltantes, deseadas y obtenidas

--- routers/skins.py
+++ routers/skins.py
@@ -16,20 +16,76 @@ def get_skins():
 #Obtener skins faltantes
 @skins.get('/skins/missing')
 def get_missing_skins():
-    skins = skins_collection.find()
+    conditions = [
+        {"isWished": False},
+        {"isOwned": False}
+    ]
+    
+    skins = skins_collection.find({
+        "$and": conditions
+    })
     convertedSkins = convertSkins(skins)
     return JSONResponse(content=jsonable_encoder(convertedSkins))
 
-#Obtener todas las skins en general
-@skins.get('/skins')
-def get_skins():
-    skins = skins_collection.find()
+#Obtener skins deseadas
+@skins.get('/skins/wished')
+def get_wished_skins():
+    conditions = [
+        {"isWished": True},
+        {"isOwned": False}
+    ]
+    
+    skins = skins_collection.find({
+        "$and": conditions
+    })
     convertedSkins = convertSkins(skins)
     return JSONResponse(content=jsonable_encoder(convertedSkins))
 
-#Obtener todas las skins en general
-@skins.get('/skins')
-def get_skins():
-    skins = skins_collection.find()
+#Obtener skins obtenidas
+@skins.get('/skins/owned')
+def get_owned_skins():
+    conditions = [
+        {"isWished": False},
+        {"isOwned": True}
+    ]
+    
+    skins = skins_collection.find({
+        "$and": conditions
+    })
     convertedSkins = convertSkins(skins)
+    return JSONResponse(content=jsonable_encoder(convertedSkins))
+
+#Añadir una skin deseada
+@skins.put('/skins/wished')
+def add_wished_skins(skin_id: str):
+    query = {"_id": ObjectId(skin_id)}
+    update = {"$set": {"isWished": True}}
+    skins_collection.update_one(query, update)
+    updated_skin = skins_collection.find_one(query)
+    convertedSkins = convertSkins(updated_skin)
+    return JSONResponse(content=jsonable_encoder(convertedSkins))
+
+#Añadir una skin obtenida
+@skins.put('/skins/owned')
+def add_owned_skins(skin_id: str):
+    query = {"_id": ObjectId(skin_id)}
+    update = {"$set": {"isOwned": True}}
+    skins_collection.update_one(query, update)
+    updated_skin = skins_collection.find_one(query)
+    convertedSkins = convertSkins(updated_skin)
+    return JSONResponse(content=jsonable_encoder(convertedSkins))
+
+#Eliminar skin
+@skins.put('/skins')
+def delete_skins(skin_id: str):
+    query = {"_id": ObjectId(skin_id)}
+    update = {
+        "$set": {
+            "isOwned": False,
+            "isWished": False
+        }
+    }
+    skins_collection.update_one(query, update)
+    updated_skin = skins_collection.find_one(query)
+    convertedSkins = convertSkins(updated_skin)
     return JSONResponse(content=jsonable_encoder(convertedSkins))
